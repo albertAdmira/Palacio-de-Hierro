546840dcab4e9e0cc52a797a034f94866aa75d73
Gagandeep Dass Kaur <gdass@admira.com>
2022-09-19 17:19:19 +0200

    Añadiendo marcas al modal

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,43 +3,44 @@ $(document).ready(function(){
 	$(".scrollBtn").click(function(){
     	$(".momentum,.banner-explorar-area").slideUp("slow");
   	});
+
 	$(".scrollBtn").click(function(){
 		$(".momentum").addClass("show");
 	});
 
+
 	var brandLogos = [
-		"acer",
-		"asus",
-		"bose",
-		"bosé",
-		"dell",
-		"echo",
-		"fitbit-luxe",
-		"fitbit-sense",
-		"garmin",
-		"google",
-		"homedics",
-		"hp",
-		"hypervolt-bluetooth",
-		"jetson",
-		"joy-resolve",
-		"kef",
-		"kindle",
-		"krups",
-		"lenovo",
-		"lg",
-		"mavic-3",
-		"miele",
-		"monitor-audio",
-		"msi",
-		"polar-pacer-pro",
-		"roku",
-		"samsung",
-		"sonos",
-		"sony",
+		"acer","asus","bose","bosé","dell","echo","fitbit-luxe",
+		"fitbit-sense","garmin","google","homedics","hp","hypervolt-bluetooth","jetson",
+		"joy-resolve","kef","kindle","krups","lenovo","lg","mavic-3","miele",
+		"monitor-audio","msi","polar-pacer-pro","roku","samsung","sonos","sony",
 	]
 
-	brandLogos.forEach(element => console.log(element));
+	$(".signature").click(function(){
+
+		brandLogos.forEach((element) => {
+
+			
+			if(element === 'acer' || element === 'Acer') {
+				element = "acer/predator";
+			}
+			
+			var finalSrc = `assets/images/${element}/${element}-logo.ai`;
+
+			console.log(finalSrc)
+
+			$(".brand-grid").append(
+				`<div class="brand-container">
+					<img src=${finalSrc} alt="${element}" data-bs-toggle="modal" data-bs-target="#brandModal" class="signature"></img>
+				</div>`
+			);
+			
+		});
+	})
+
+	$("#close-modal").click(function(){
+	})
+
 	
 });
 
